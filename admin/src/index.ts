3a348aa28c2a4ebef4a04305369a4471636b16ce
Sameh Hammad Ahmed <sameh.ahmed@mhg-int.com>
2024-12-08 16:10:17 +0200

    ddd

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -10,7 +10,7 @@ export default {
       icon: PluginIcon,
       intlLabel: {
         id: `${PLUGIN_ID}.plugin.name`,
-        defaultMessage: PLUGIN_ID,
+        defaultMessage: "Excel To JSON Converter",
       },
       Component: async () => {
         const { App } = await import('./pages/App');
